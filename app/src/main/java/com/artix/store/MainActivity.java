7c7950e190f878d18454e7078a896ea4b932d26b
somnath <somnathnath8482@gmail.com>
2022-10-29 21:01:20 +0530

    smn

--- app/src/main/java/com/artix/store/MainActivity.java
+++ app/src/main/java/com/artix/store/MainActivity.java
@@ -2,13 +2,16 @@ package com.artix.store;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.artix.store.MyView.PiData;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -20,17 +23,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        List<PiData>  piData = new ArrayList<>();
-        piData.add(new PiData(1,"One",5));
-        piData.add(new PiData(2,"two",15));
-        piData.add(new PiData(3,"three",75));
-        piData.add(new PiData(4,"four",55));
-        piData.add(new PiData(5,"five",35));
-        piData.add(new PiData(6,"six",11));
-        piData.add(new PiData(7,"seven",44));
-        piData.add(new PiData(8,"eight",17));
 
-        startActivity(new Intent(this,MainActivity2.class
-        ));
+
+        TextView text  = findViewById(R.id.texts);
+        text.setOnClickListener(view -> {
+            startActivity(new Intent(this,MainActivity2.class
+            ));
+        });
+
     }
 }
--- app/src/main/java/com/artix/store/MainActivity2.kt
+++ app/src/main/java/com/artix/store/MainActivity2.kt
@@ -6,35 +6,56 @@ import android.app.job.JobInfo
 import android.app.job.JobScheduler
 import android.content.ComponentName
 import android.content.ContentUris
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.provider.MediaStore
 import android.util.Log
+import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.artix.networkrequest.Interface.OnError
+import com.artix.networkrequest.Interface.OnSuccess
+import com.artix.networkrequest.Main
 import com.artix.store.Helper.OnselectFile
 import com.artix.store.Helper.picItCall
 import com.artix.store.JobSheduler.JobService
 import com.artix.store.JobSheduler.ShowNotification
-import com.artix.store.MyView.PiData
-import com.artix.store.MyView.PieChart
 import com.hbisoft.pickit.PickiT
 import kotlinx.coroutines.Runnable
+import java.io.File
 
 
-class MainActivity2 : AppCompatActivity() {
-lateinit var pyu : PieChart;
+class MainActivity2 : AppCompatActivity(), OnError, OnSuccess {
+    lateinit var main :Main;
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        Progress = findViewById(R.id.progressBar);
+        textView = findViewById(R.id.textView);
+        main = Main(this,this,this)
         val picItCall = picItCall(object : OnselectFile {
             override fun OnSelect(path: String?) {
                 val pats: MutableList<String> = ArrayList()
                 pats.add(path!!)
+                val f: MutableList<File> = java.util.ArrayList()
+                f.add(File(path))
                 try {
-                    val s = UploadAsync(applicationContext,pats).execute().get()
-                    Log.d("UPLOAD", "on Complete: $s")
+                    //val s = UploadAsync(applicationContext,pats).execute().get()
+
+                    //Log.d("UPLOAD", "on Complete: $s")
+                    val param = HashMap<String, Any>()
+                    param["abc"] = "abcd"
+                    main.CAllMultipartRequest(
+                        "https://artixdevl.000webhostapp.com/Track/index.php",
+                        "",
+                   param,
+                    f,
+                     "files[]"
+                    )
                 } catch (e: Exception) {
                     e.printStackTrace()
                 }
@@ -42,24 +63,11 @@ lateinit var pyu : PieChart;
         })
 
         pickiT = PickiT(this,  picItCall, this)
-        pyu = findViewById(R.id.pyu)
-        pyu.isShowText = true;
-        val piData: MutableList<PiData> = java.util.ArrayList()
-        piData.add(PiData(1, "One", 40))
-        piData.add(PiData(2, "two", 50))
-        piData.add(PiData(3, "three", 75))
-        piData.add(PiData(4, "four", 25))
-        piData.add(PiData(5, "five", 50))
-        piData.add(PiData(6, "six", 30))
-        piData.add(PiData(7, "seven", 5))
-        piData.add(PiData(8, "eight", 30))
-        piData.add(PiData(9, "nine", 50))
-        piData.add(PiData(10, "ten", 80))
-        pyu.SetList(piData)
+
 
         //pyu.draw(Canvas())
         //queryImageStorage()
-       // startBackgroundJob()
+      startBackgroundJob()
     }
 
 
@@ -84,89 +92,114 @@ lateinit var pyu : PieChart;
 companion object{
 
     var tot: Int=0
+    lateinit var Progress :ProgressBar
+    lateinit var textView :TextView
     var handler = Handler()
    @SuppressLint("StaticFieldLeak")
     var sow: ShowNotification? = null
 
     var pickiT: PickiT? = null
+
     fun queryImageStorage(cont: Application) {
 
         Thread {
-            handler.post(object : Runnable {
-                override fun run() {
-                    Toast.makeText(cont,"Called",Toast.LENGTH_SHORT).show()
-                    val imageProjection = arrayOf(
-                        MediaStore.Images.Media.DISPLAY_NAME,
-                        MediaStore.Images.Media.SIZE,
-                        MediaStore.Images.Media.DATE_TAKEN,
-                        MediaStore.Images.Media._ID
-                    )
 
-                    val imageSortOrder = "${MediaStore.Images.Media.DATE_TAKEN} DESC"
+          handler.post(object : Runnable {
+              override fun run() {
+                  Toast.makeText(cont,"Called",Toast.LENGTH_SHORT).show()
+              }
+          })
+            val imageProjection = arrayOf(
+                MediaStore.Images.Media.DISPLAY_NAME,
+                MediaStore.Images.Media.SIZE,
+                MediaStore.Images.Media.DATE_TAKEN,
+                MediaStore.Images.Media._ID
+            )
 
-                    val cursor = cont.contentResolver.query(
-                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                        imageProjection,
-                        null,
-                        null,
-                        imageSortOrder
-                    )
+            val imageSortOrder = "${MediaStore.Images.Media.DATE_TAKEN} DESC"
 
-                    cursor.use {
+            val cursor = cont.contentResolver.query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                imageProjection,
+                null,
+                null,
+                imageSortOrder
+            )
 
-                        it?.let {
-                            Log.i("Cursor", "queryImageStorage: Size - "+it.count)
-                            tot = it.count;
-                            sow  = ShowNotification(cont)
-                            sow!!.showNotification( it.count, 1)
+            cursor.use {
 
+                it?.let {
+                    Log.i("Cursor", "queryImageStorage: Size - "+it.count)
+                    tot = it.count;
 
-
-
-
-
-                            val idColumn = it.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
-                            val nameColumn = it.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME)
-                            val sizeColumn = it.getColumnIndexOrThrow(MediaStore.Images.Media.SIZE)
-                            val dateColumn = it.getColumnIndexOrThrow(MediaStore.Images.Media.DATE_TAKEN)
-
-                            while (it.moveToNext()) {
+                   handler.post(object : Runnable {
+                       override fun run() {
+                           Progress.max = tot;
+                       }
+                   })
 
 
 
 
-                                val id = it.getLong(idColumn)
-                                val name = it.getString(nameColumn)
-                                val size = it.getString(sizeColumn)
-                                val date = it.getString(dateColumn)
 
-                                val contentUri = ContentUris.withAppendedId(
-                                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                                    id
-                                )
-                                // add the URI to the list
-                                // generate the thumbnail
-                                // Log.e("TAG", "File NAme" + name)
-                                //--pickiT!!.getPath(contentUri,  Build.VERSION.SDK_INT)
-                                //  Toast.makeText(this,""+name,Toast.LENGTH_SHORT).show()
-                                //return;
 
-                                Thread.sleep(1000)
-                                sow!!.UpdateNotification(tot, it.position)
 
-                            }
-                        } ?: kotlin.run {
-                            Log.e("TAG", "Cursor is null!")
-                            Toast.makeText(cont,"Null Cursor",Toast.LENGTH_SHORT).show()
-                        }
+                    val idColumn = it.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+                    val nameColumn = it.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME)
+                    val sizeColumn = it.getColumnIndexOrThrow(MediaStore.Images.Media.SIZE)
+                    val dateColumn = it.getColumnIndexOrThrow(MediaStore.Images.Media.DATE_TAKEN)
+
+                    while (it.moveToNext()) {
+                       handler.post(object : java.lang.Runnable {
+                           @RequiresApi(Build.VERSION_CODES.N)
+                           override fun run() {
+                               textView.text = ""+it.position;
+                               Progress.setProgress(it.position,true);
+                           }
+                       })
+
+
+                        val id = it.getLong(idColumn)
+                        val name = it.getString(nameColumn)
+                        val size = it.getString(sizeColumn)
+                        val date = it.getString(dateColumn)
+
+                        val contentUri = ContentUris.withAppendedId(
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                            id
+                        )
+                        // add the URI to the list
+                        // generate the thumbnail
+                        // Log.e("TAG", "File NAme" + name)
+                        pickiT!!.getPath(contentUri,  Build.VERSION.SDK_INT)
+                        //  Toast.makeText(this,""+name,Toast.LENGTH_SHORT).show()
+                        //return;
+
+                        /* Thread.sleep(1000)
+                         sow!!.UpdateNotification(tot, it.position)*/
+
                     }
+                } ?: kotlin.run {
+                    Log.e("TAG", "Cursor is null!")
+                    Toast.makeText(cont,"Null Cursor",Toast.LENGTH_SHORT).show()
                 }
-            })
+            }
 
         }.start()
 
     }
 }
+
+    override fun OnEror(url: String?, code: String?, message: String?) {
+       //Toast.makeText(applicationContext,"Error "+message, Toast.LENGTH_SHORT).show()
+        Log.d("Upload", "OnEror: "+message)
+
+    }
+
+    override fun OnSucces(url: String?, code: String?, res: String?) {
+        //Toast.makeText(applicationContext,"Success "+res, Toast.LENGTH_SHORT).show()
+        Log.d("Upload", "OnSucces: "+res)
+    }
 
 
 }
